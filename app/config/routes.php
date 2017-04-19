705be75c77aca0f8da60efb3526ba8387509efd8
miquelator <miquelet-sans@hotmail.com>
2017-04-19 19:41:00 +0200

    Update routes.php

--- app/config/routes.php
+++ app/config/routes.php
@@ -2,5 +2,6 @@
 
 $app->get('/hello/', 'PracticaFinal\\Controller\\HelloController::indexAction');
 $app->get('add/{num1}/{num2}','SilexApp\\Controller\\HelloController::addAction');
-
+$app->get('/users/get/{id}','PracticaFinal\Controller\UserController::getAction');
+$app->match('/users/add','PracticaFinal\Controller\UserController::postAction');
 ?>
