0d43b07c638bb0f4fde85a17e3b9a34ce2c46656
miquelator <miquelet-sans@hotmail.com>
2017-04-20 19:46:37 +0200

    fsf

--- app/config/routes.php
+++ app/config/routes.php
@@ -1,5 +1,5 @@
 <?php
-//prova nickkiyotfututyruftftu
+//prova holaaa
 $app->get('/hello/', 'PracticaFinal\\Controller\\HelloController::indexAction');
 $app->get('add/{num1}/{num2}','SilexApp\\Controller\\HelloController::addAction');
 $app->get('/users/get/{id}','PracticaFinal\Controller\UserController::getAction');
