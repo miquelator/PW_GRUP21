b20023697e5e94efedb9323abc277fa9bd915545
Nick Van der Graaf <Nick@MacBook-Pro-de-Nick.local>
2017-04-20 19:19:51 +0200

    desto

--- app/config/routes.php
+++ app/config/routes.php
@@ -2,5 +2,7 @@
 //prova nickkiyotfututyruftftu
 $app->get('/hello/', 'PracticaFinal\\Controller\\HelloController::indexAction');
 $app->get('add/{num1}/{num2}','SilexApp\\Controller\\HelloController::addAction');
-
+$app->get('/users/get/{id}','PracticaFinal\Controller\UserController::getAction');
+$app->match('/users/add','PracticaFinal\Controller\UserController::postAction');
+//convis miquel
 ?>
