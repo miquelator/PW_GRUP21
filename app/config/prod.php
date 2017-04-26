460a04e21b0fdc11c634fc068a9c845cf1d137f4
miquelator <miquelet-sans@hotmail.com>
2017-04-26 18:52:51 +0200

    fsf

--- app/config/prod.php
+++ app/config/prod.php
@@ -10,15 +10,14 @@ $app->register(new Silex\Provider\DoctrineServiceProvider(),array(
 		'password'=>'root'),
 	));
 
-$app->register(new Silex\Provider\AssetServiceProvider(),array(
-
-	'assets.vesion'=>'v1',
-	'assets.version_format'=>'%s?version=%s',
-	'assets.named_packages'=>array(
-		'css'=>array('base_path' => '/assets/css'),
-		'js' => array('base_path' => '/assets/js'),
-		'images' => array('base_urls'=>array('http://silexapp.dev/assets/img')),
-		),
+$app->register(new Silex\Provider\AssetServiceProvider(), array(
+    'assets.version' => 'v1',
+    'assets.version_format' => '%s?version=%s',
+    'assets.named_packages' => array(
+        'css' => array('base_path' => '/assets/css'),
+        'js' => array('base_path' => '/assets/js'),
+        'images' => array('base_urls' => array('http://silexapp.dev/assets/img')),
+    ),
 ));
 $app->register(new \PracticaFinal\Providers\HelloServiceProvider(),array(
     'hello.default_name' => 'Bullshit2',
