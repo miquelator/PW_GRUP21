838515d4674270b93440fde8c495473b05f48299
miquelator <miquelet-sans@hotmail.com>
2017-05-14 19:26:25 +0200

    fsf

--- src/Controller/DatabaseController.php
+++ src/Controller/DatabaseController.php
@@ -56,6 +56,26 @@ class DatabaseController{
         }
     }
 
+    public function searchUser (Application $app, Request $request){
+        $response = new Response();
+        $name=$request->get('name');
+        $password=$request->get('password');
+
+        try {
+            $sql= "SELECT * FROM user WHERE (username = ? or email = ?) and password = ?  ORDER BY id DESC LIMIT 1";
+            $info = $app['db']->fetchAssoc($sql, array ((string) $name,(string) $name,(string)$password));
+        }catch (Exception $e) {
+            $response->setStatusCode(Response::HTTP_BAD_REQUEST);
+            $content = $app['twig']->render('main_register.twig', [
+                'errors' => [
+                    'unexpected' => 'An error has occurred, please try it again later'
+                ]
+            ]);
+            $response->setContent($content);
+            return $response;
+        }
+    }
+
 
 
 
--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -126,10 +126,22 @@ class UserController{
 
         $name = $request->get('name');
         $password = $request->get('password');
+        try {
+            $sql= "SELECT * FROM user WHERE (username = ? or email = ?) and password = ?  ORDER BY id DESC LIMIT 1";
+            $info = $app['db']->fetchAssoc($sql, array ((string) $name,(string) $name,(string)$password));
+            echo $info;
+            $content = $app['twig']->render('showUser.twig',array('name' => $name));
 
+        }catch (Exception $e) {
+            $response->setStatusCode(Response::HTTP_BAD_REQUEST);
+            $content = $app['twig']->render('home.twig', [
+                'errors' => [
+                    'unexpected' => 'An error has occurred, please try it again later'
+                ]
+            ]);
+        }
         $response->setStatusCode(Response::HTTP_OK);
 
-        $content = $app['twig']->render('showUser.twig',array('name' => $name,'password'=>$password));
         $response->setContent($content);
         return $response;
 
